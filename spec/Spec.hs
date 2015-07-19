be9a5ca9f9311eb9790e0dccfa830b2290f5c8cb
Milán Nagy <123.wizek@gmail.com>
2015-07-19 19:37:54 +0200

    everything passes

--- spec/Spec.hs
+++ spec/Spec.hs
@@ -7,10 +7,6 @@ import Internal.Parse
 import Text.InterpolatedString.Perl6
 import Utils
 
--- xdescribe n = describe n $ it "" pending
--- main = pTest
--- main = print 123
-
 main = spec
 
 spec = hspec $ do
@@ -75,29 +71,28 @@ spec = hspec $ do
       separate "a ',' b" `shouldBe` ["a ',' b"]
       separate "(a, b \")\"), b" `shouldBe` ["(a, b \")\")", " b"]
 
-  -- describe "Debug.Dump" $ do
-  --   -- TODO decide if this is useful enough to warrant support
-  --   -- it "should execute even if empty" $ do
-  --   --   [d||] `shouldBe` ""
+  describe "Debug.Dump" $ do
+    -- TODO decide if this is useful enough to warrant support
+    -- it "should execute even if empty" $ do
+    --   [d||] `shouldBe` ""
 
-  --   it "should work with single literal" $ do
-  --     [d|1|] `shouldBe` "(1) = 1"
+    it "should work with single literal" $ do
+      [d|1|] `shouldBe` "(1) = 1"
 
-  --   it "should work with simple expression" $ do
-  --     [d|1 + 1|] `shouldBe` "(1 + 1) = 2"
+    it "should work with simple expression" $ do
+      [d|1 + 1|] `shouldBe` "(1 + 1) = 2"
 
-  --   it "should work with bindings" $ do
-  --     let a = 1
-  --     [d|a|] `shouldBe` "(a) = 1"
-  --     [d|a + 1|] `shouldBe` "(a + 1) = 2"
+    it "should work with bindings" $ do
+      let a = 1
+      [d|a|] `shouldBe` "(a) = 1"
+      [d|a + 1|] `shouldBe` "(a + 1) = 2"
 
-  --   it "should work with comma separated expressions" $ do
-  --     [d|1, 2|] `shouldBe` "(1) = 1, (2) = 2"
-  --     let i = 0.25
-  --     let f = (1 -)
-  --     [d|i, 1/i, f i, f (1/i)|] `shouldBe`
-  --       "(i) = 0.25, (1/i) = 4.0, (f i) = 0.75, (f (1/i)) = -3.0"
+    it "should work with comma separated expressions" $ do
+      [d|1, 2|] `shouldBe` "(1) = 1\t(2) = 2"
+      let i = 0.25
+      let f = (1 -)
+      [d|i, 1/i, f i, f (1/i)|] `shouldBe`
+        "(i) = 0.25\t(1/i) = 4.0\t(f i) = 0.75\t(f (1/i)) = -3.0"
 
-  --   -- TODO parser, WIP on 'parse' branch
-  --   -- it "should step over commas in sub-expressions" $ do
-  --   --   [d|(1, 2), 1|] `shouldBe` "((1, 1)) = (1,1), (1) = 1"
+    it "should step over commas in sub-expressions" $ do
+      [d|(1, 2), 1|] `shouldBe` "((1, 2)) = (1,2)\t(1) = 1"
