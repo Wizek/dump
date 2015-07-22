40fdb7c4c7dbadbe9055fe26361d38e9e8117f2f
Milán Nagy <123.wizek@gmail.com>
2015-07-22 10:24:31 +0200

    experimenting with unifying two pieces of code

--- src/Internal/Parser.hs
+++ src/Internal/Parser.hs
@@ -4,10 +4,17 @@ import Debug.Trace
 
 splitOnCommas :: String -> [String]
 splitOnCommas "" = []
-splitOnCommas str = match : splitOnCommas rest
+splitOnCommas str = fff str
+
+fff str = match : splitOnCommas rest
   where
-    match = parseExpr str
+    match = parseExprUntil ',' str
     rest = drop (length match + 1) str
+
+fff' c xs thenParse parseUntil cc = match ++ thenParse rest
+  where
+    match = parseUntil cc xs
+    rest  = drop (length match) xs
 
 parseExpr :: String -> String
 parseExpr = parseExprUntil ','
@@ -24,11 +31,9 @@ parseExprUntil c (x : xs)
     where
       recurseUntil = parseWith parseExprUntil
       leafUntil    = parseWith parseLeafUntil
-      -- TODO parseWith parseUntil c xs cc = match ++ parseExprUntil c rest
-      parseWith parseUntil cc = match ++ parseExprUntil c rest
-        where
-          match = parseUntil cc xs
-          rest  = drop (length match) xs
+      parseWith    = fff' c xs (parseExprUntil c)
+
+-- parseWith parseUntil cc = match ++ parseExprUntil c rest
 
 parseLeafUntil :: Char -> String -> String
 parseLeafUntil _  ""   = ""
