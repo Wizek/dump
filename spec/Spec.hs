4d1e07ae0902932b3b2d40dd1471294bcbae08a8
Milán Nagy <123.wizek@gmail.com>
2015-07-24 10:21:06 +0200

    parseLeaf works, the rest is WIP

--- spec/Spec.hs
+++ spec/Spec.hs
@@ -20,101 +20,104 @@ spec = hspec $ do
       wrapInParens "a" `shouldBe` "(a)"
       wrapInParens "" `shouldBe` "()"
 
-  describe "parseExpr" $ do
-    let p = parseExpr
-    it "handles flat" $ do
-      p "asd" `shouldBe` "asd"
-      p "asd,asd" `shouldBe` "asd"
-      p "asd,a,sd" `shouldBe` "asd"
-
-    it "handles parens" $ do
-      p "(a,b)" `shouldBe` "(a,b)"
-      p "(a,b) (a,b)" `shouldBe` "(a,b) (a,b)"
-      p "(a,b), c" `shouldBe` "(a,b)"
-      p "((a), b), c" `shouldBe` "((a), b)"
-      p "((a,b),(a,b)), a" `shouldBe` "((a,b),(a,b))"
-
-    it "handles char literals" $ do
-      p "','" `shouldBe` "','"
-      p "',', a" `shouldBe` "','"
-
-    it "handles string literals" $ do
-      p [q|","|] `shouldBe` [q|","|]
-      p [q|",", a|] `shouldBe` [q|","|]
-
-    it "handles list literals" $ do
-      p "[a,b]" `shouldBe` "[a,b]"
-
-    it "treats string literals as leaves" $ do
-      p [q|"(", a|] `shouldBe` [q|"("|]
-
-    it "support escaping in leaves" $ do
-      p [q|"\"", a|] `shouldBe` [q|"\""|]
-      p [q|"a" ++ "b", a|] `shouldBe` [q|"a" ++ "b"|]
-
-    it "misc" $ do
-      p [q|"((", a|] `shouldBe` [q|"(("|]
-      p [q|"()(", a|] `shouldBe` [q|"()("|]
-      p "(a, )" `shouldBe` "(a, )"
-      p "(, b)" `shouldBe` "(, b)"
-
-    it "shouldn't modify the list" $ do
-      p "[" `shouldBe` "["
-      p "([)]" `shouldBe` "([)]"
-      -- p "([)], a" `shouldBe` "([)]"
-
-    it "shouldn't modify the list (QuickCheck)" $ property $ let
-      prop :: String -> Property
-      prop s = parseExpr s `isPrefixOf` s
-        $> counterexample [d|show s, parseExpr s|]
-      in prop
-
   describe "parseLeafUntil" $ do
-    let p = parseLeafUntil
+    let p = parseLeafUntil '"'
     it "knows to stop on regular quote" $ do
-      p '"' [q|a", b|] `shouldBe` [q|a"|]
+      p [q|a"   |] `shouldBe` ([q|a"|], "   ")
 
     it "knows not to terminate after escaped quote" $ do
-      p '"' [q|a\"", b|] `shouldBe` [q|a\""|]
+      p [q|a\""   |] `shouldBe` ([q|a\""|], "   ")
 
     it "does not trip up on escape characters" $ do
-      p '"' [q|a\\ b|] `shouldBe` [q|a\\ b|]
+      p [q|a\\  |] `shouldBe` ([q|a\\  |], "")
 
-  describe "splitOnCommas" $ do
-    it "should work" $ do
-      let s = splitOnCommas
-      s "a" `shouldBe` ["a"]
-      s "a,b" `shouldBe` ["a", "b"]
-      s "a, b" `shouldBe` ["a", " b"]
-      s "(a)" `shouldBe` ["(a)"]
-      s "(a, b)" `shouldBe` ["(a, b)"]
-      s "(a, b), b" `shouldBe` ["(a, b)", " b"]
-      s "','" `shouldBe` ["','"]
-      s "a ',' b" `shouldBe` ["a ',' b"]
-      s "(a, b \")\"), b" `shouldBe` ["(a, b \")\")", " b"]
+    it "does not trip up on escape character at the end" $ do
+      p [q|a\\|] `shouldBe` ([q|a\\|], "")
 
-  describe "Debug.Dump" $ do
-    -- TODO decide if this is useful enough to warrant support
-    -- it "should execute even if empty" $ do
-    --   [d||] `shouldBe` ""
+  -- describe "parseExpr" $ do
+  --   let p = parseExpr .> fst
+  --   it "handles flat" $ do
+  --     p "asd" `shouldBe` "asd"
+  --     p "asd,asd" `shouldBe` "asd"
+  --     p "asd,a,sd" `shouldBe` "asd"
 
-    it "should work with single literal" $ do
-      [d|1|] `shouldBe` "(1) = 1"
+  --   it "handles parens" $ do
+  --     p "(a,b)" `shouldBe` "(a,b)"
+  --     p "(a,b) (a,b)" `shouldBe` "(a,b) (a,b)"
+  --     p "(a,b), c" `shouldBe` "(a,b)"
+  --     p "((a), b), c" `shouldBe` "((a), b)"
+  --     p "((a,b),(a,b)), a" `shouldBe` "((a,b),(a,b))"
 
-    it "should work with simple expression" $ do
-      [d|1 + 1|] `shouldBe` "(1 + 1) = 2"
+  --   it "handles char literals" $ do
+  --     p "','" `shouldBe` "','"
+  --     p "',', a" `shouldBe` "','"
 
-    it "should work with bindings" $ do
-      let a = 1
-      [d|a|] `shouldBe` "(a) = 1"
-      [d|a + 1|] `shouldBe` "(a + 1) = 2"
+  --   it "handles string literals" $ do
+  --     p [q|","|] `shouldBe` [q|","|]
+  --     p [q|",", a|] `shouldBe` [q|","|]
 
-    it "should work with comma separated expressions" $ do
-      [d|1, 2|] `shouldBe` "(1) = 1\t  (2) = 2"
-      let i = 0.25
-      let f = (1 -)
-      [d|i, 1/i, f i, f (1/i)|] `shouldBe`
-        "(i) = 0.25\t  (1/i) = 4.0\t  (f i) = 0.75\t  (f (1/i)) = -3.0"
+  --   it "handles list literals" $ do
+  --     p "[a,b]" `shouldBe` "[a,b]"
 
-    it "should step over commas in sub-expressions" $ do
-      [d|(1, 2), 1|] `shouldBe` "((1, 2)) = (1,2)\t  (1) = 1"
+  --   it "treats string literals as leaves" $ do
+  --     p [q|"(", a|] `shouldBe` [q|"("|]
+
+  --   it "support escaping in leaves" $ do
+  --     p [q|"\"", a|] `shouldBe` [q|"\""|]
+  --     p [q|"a" ++ "b", a|] `shouldBe` [q|"a" ++ "b"|]
+
+  --   it "misc" $ do
+  --     p [q|"((", a|] `shouldBe` [q|"(("|]
+  --     p [q|"()(", a|] `shouldBe` [q|"()("|]
+  --     p "(a, )" `shouldBe` "(a, )"
+  --     p "(, b)" `shouldBe` "(, b)"
+
+  --   it "shouldn't modify the list" $ do
+  --     p "[" `shouldBe` "["
+  --     p "([)]" `shouldBe` "([)]"
+  --     -- p "([)], a" `shouldBe` "([)]"
+
+  --   it "shouldn't modify the list (QuickCheck)" $ property $ let
+  --     prop :: String -> Property
+  --     prop s = parseExpr s `isPrefixOf` s
+  --       $> counterexample [d|show s, parseExpr s|]
+  --     in prop
+
+  -- describe "splitOnCommas" $ do
+  --   it "should work" $ do
+  --     let s = splitOnCommas
+  --     s "a" `shouldBe` ["a"]
+  --     s "a,b" `shouldBe` ["a", "b"]
+  --     s "a, b" `shouldBe` ["a", " b"]
+  --     s "(a)" `shouldBe` ["(a)"]
+  --     s "(a, b)" `shouldBe` ["(a, b)"]
+  --     s "(a, b), b" `shouldBe` ["(a, b)", " b"]
+  --     s "','" `shouldBe` ["','"]
+  --     s "a ',' b" `shouldBe` ["a ',' b"]
+  --     s "(a, b \")\"), b" `shouldBe` ["(a, b \")\")", " b"]
+
+  -- describe "Debug.Dump" $ do
+  --   -- TODO decide if this is useful enough to warrant support
+  --   -- it "should execute even if empty" $ do
+  --   --   [d||] `shouldBe` ""
+
+  --   it "should work with single literal" $ do
+  --     [d|1|] `shouldBe` "(1) = 1"
+
+  --   it "should work with simple expression" $ do
+  --     [d|1 + 1|] `shouldBe` "(1 + 1) = 2"
+
+  --   it "should work with bindings" $ do
+  --     let a = 1
+  --     [d|a|] `shouldBe` "(a) = 1"
+  --     [d|a + 1|] `shouldBe` "(a + 1) = 2"
+
+  --   it "should work with comma separated expressions" $ do
+  --     [d|1, 2|] `shouldBe` "(1) = 1\t  (2) = 2"
+  --     let i = 0.25
+  --     let f = (1 -)
+  --     [d|i, 1/i, f i, f (1/i)|] `shouldBe`
+  --       "(i) = 0.25\t  (1/i) = 4.0\t  (f i) = 0.75\t  (f (1/i)) = -3.0"
+
+  --   it "should step over commas in sub-expressions" $ do
+  --     [d|(1, 2), 1|] `shouldBe` "((1, 2)) = (1,2)\t  (1) = 1"
--- src/Internal/Parser.hs
+++ src/Internal/Parser.hs
@@ -1,3 +1,5 @@
+{-# OPTIONS_GHC -fdefer-type-errors #-}
+
 module Internal.Parser where
 
 import Debug.Trace
@@ -11,35 +13,51 @@ fff str = match : splitOnCommas rest
     match = parseExprUntil ',' str
     rest = drop (length match + 1) str
 
-fff' c xs thenParse parseUntil cc = match ++ thenParse rest
-  where
-    match = parseUntil cc xs
-    rest  = drop (length match) xs
-
 parseExpr :: String -> String
 parseExpr = parseExprUntil ','
 
-parseExprUntil :: Char -> String -> String
-parseExprUntil _ "" = ""
+
+-- splitAt _ []     = ([], [])
+-- splitAt 0 xs     = ([], xs)
+-- splitAt n (x:xs) = (x:m, r)
+--     where (m, r) = splitAt (n-1) xs
+
+-- groupsOf :: Int -> [a] -> [[a]]
+-- groupsOf n [] = []
+-- groupsOf n xs = m : groupsOf n r
+--     where (m, r) = splitAt n xs
+
+-- groupsOf 2 [1..5]
+
+parseExprUntil :: Char -> String -> (String, String)
+parseExprUntil _ "" = ("", "")
 parseExprUntil c (x : xs)
-  | x == c      = if c == ',' then "" else [x]
-  | x == '('    = x : recurseUntil ')'
-  | x == '['    = x : recurseUntil ']'
-  | x == '\''   = x : leafUntil '\''
-  | x == '"'    = x : leafUntil '"'
-  | otherwise   = x : parseExprUntil c xs
+  | x == c    = if c == ',' then "" else [x]
+  | otherwise =
+    x : case x of
+      '('  -> recurseUntil ')'
+      '['  -> recurseUntil ']'
+      '\'' -> leafUntil '\''
+      '"'  -> leafUntil '"'
+      _    -> parseExprUntil c xs
     where
       recurseUntil = parseWith parseExprUntil
       leafUntil    = parseWith parseLeafUntil
-      parseWith    = fff' c xs (parseExprUntil c)
+      -- parseWith    = fff' c xs (parseExprUntil c)
+      -- fff' c xs thenParse parseUntil cc = match ++ thenParse rest
+      parseWith parseUntil cc = match ++ parseExprUntil c rest
+        where
+          match = parseUntil cc xs
+          rest  = drop (length match) xs
 
--- parseWith parseUntil cc = match ++ parseExprUntil c rest
-
-parseLeafUntil :: Char -> String -> String
-parseLeafUntil _  ""   = ""
+parseLeafUntil :: Char -> String -> (String, String)
+parseLeafUntil _  "\\" = ("\\", "")
+parseLeafUntil _  ""   = ("",   "")
 parseLeafUntil cc (x:xs)
-  | x == cc   = [x]
-  | x == '\\' = x : case xs of
-    ""       -> ""
-    (y:ys)   -> y : parseLeafUntil cc ys
-  | otherwise = x : parseLeafUntil cc xs
+  | x == cc   = ([x], xs)
+  | x == '\\' = let (y:ys) = xs in
+    putMatch x $ putMatch y $ parseLeafUntil cc ys
+  | otherwise = putMatch x $ parseLeafUntil cc xs
+
+putMatch :: a -> ([a], [b]) -> ([a], [b])
+putMatch x (m, r) = (x:m, r)
